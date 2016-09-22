8bb24e7d4ba3475464ceabf7325330ca2ba9d626
animomin <animomin@daum.net>
2016-09-23 00:06:29 +0900

    records.data null check

--- public/javascripts/as/neo.manage.js
+++ public/javascripts/as/neo.manage.js
@@ -147,49 +147,60 @@
                 success: function(opt, records) {
                     var $table = _this.elem.$table;
 
-                    records.data.forEach(function(_item, _index){
-                        var temp = ASSTATUS.ServiceBackGround(_item.서비스상태);
+                    if (records.data && records.data.length) {
+                        records.data.forEach(function(_item, _index) {
+                            var temp = ASSTATUS.ServiceBackGround(_item.서비스상태);
                             temp.value = _item;
                             records.data[_index] = JSON.parse(JSON.stringify(temp));
-                    });
+                        });
+                    }
 
                     $table.footable({
-                        "columns" : [
-                            { 'name' : '인덱스' , 'title' : 'ID', 'style' : { 'width' : 50, 'maxWidth' : 50 } },
-                            { 'name' : '기관코드', 'title' : '기관번호' },
-                            { 'name' : '기관명칭', 'title' : '기관명칭' },
-                            { 'name' : '프로그램', 'title' : '프로그램',
-                                formatter : function(value){
-                                    if(value === "" || typeof value === "undefined") value = 0;
-                                    switch(parseInt(value)){
-                                        case 1: return "<span class='badge badge-eplus'>Eplus</span>";
-                                        case 8: return "<span class='badge badge-medi'>Medi</span>";
-                                        case 20 : return "<span class='badge badge-sense'>Sense</span>";
+                        "columns": [
+                            { 'name': '인덱스', 'title': 'ID', 'style': { 'width': 50, 'maxWidth': 50 } },
+                            { 'name': '기관코드', 'title': '기관번호' },
+                            { 'name': '기관명칭', 'title': '기관명칭' },
+                            {
+                                'name': '프로그램',
+                                'title': '프로그램',
+                                formatter: function(value) {
+                                    if (value === "" || typeof value === "undefined") value = 0;
+                                    switch (parseInt(value)) {
+                                        case 1:
+                                            return "<span class='badge badge-eplus'>Eplus</span>";
+                                        case 8:
+                                            return "<span class='badge badge-medi'>Medi</span>";
+                                        case 20:
+                                            return "<span class='badge badge-sense'>Sense</span>";
                                     }
                                 }
                             },
-                            { 'name' : '지사코드', 'title' : '담당지사',
-                                formatter : function(value){
-                                    if(value === '' || typeof value === 'undefined') value = '0000';
+                            {
+                                'name': '지사코드',
+                                'title': '담당지사',
+                                formatter: function(value) {
+                                    if (value === '' || typeof value === 'undefined') value = '0000';
                                     return neo.area[value];
                                 }
                             },
-                            { 'name' : '서비스상태' , 'title' : '상태',
-                                formatter : function(value){
+                            {
+                                'name': '서비스상태',
+                                'title': '상태',
+                                formatter: function(value) {
                                     value = parseInt(value);
                                     return ASSTATUS.ServiceName(value);
                                 }
                             },
-                            { 'name' : '접수자' , 'title' : '접수자' },
-                            { 'name' : '접수일자' , 'title' : '접수일', 'type' : 'date', 'formatString' : 'YYYY-MM-DD' },
-                            { 'name' : '확인자' , 'title' : '확인자' },
-                            { 'name' : '확인일자' , 'title' : '확인일', 'type' : 'date', 'formatString' : 'YYYY-MM-DD' },
-                            { 'name' : '인계자' , 'title' : '인계자' },
-                            { 'name' : '인계일자' , 'title' : '인계일', 'type' : 'date', 'formatString' : 'YYYY-MM-DD' },
-                            { 'name' : '처리자' , 'title' : '처리자' },
-                            { 'name' : '처리일자' , 'title' : '처리일', 'type' : 'date', 'formatString' : 'YYYY-MM-DD' },
+                            { 'name': '접수자', 'title': '접수자' },
+                            { 'name': '접수일자', 'title': '접수일', 'type': 'date', 'formatString': 'YYYY-MM-DD' },
+                            { 'name': '확인자', 'title': '확인자' },
+                            { 'name': '확인일자', 'title': '확인일', 'type': 'date', 'formatString': 'YYYY-MM-DD' },
+                            { 'name': '인계자', 'title': '인계자' },
+                            { 'name': '인계일자', 'title': '인계일', 'type': 'date', 'formatString': 'YYYY-MM-DD' },
+                            { 'name': '처리자', 'title': '처리자' },
+                            { 'name': '처리일자', 'title': '처리일', 'type': 'date', 'formatString': 'YYYY-MM-DD' },
                         ],
-                        "rows" : records.data
+                        "rows": records.data
                     });
                 },
                 callback: function() {
