5e4690085378c0c4c6494ed47620bc87b3d079c1
animomin <animomin@daum.net>
2016-11-04 00:07:38 +0900

    push bug fixed2

--- bin/notifier.js
+++ bin/notifier.js
@@ -17,6 +17,7 @@ exports.sendPush = function(type, area, summary, callback){
   var _summary = '';
   switch (type) {
     case 'JOIN':
+
       body = '새로운 A/S 요청이 접수되었습니다.\n' + summary + '\nhttp://115.68.114.16:4183';
       break;
     case 'STATUS':
@@ -51,9 +52,9 @@ exports.sendPush = function(type, area, summary, callback){
   if(area === '0036') area = '0025';
   if(area === '0037') area = '0033';
 
-  if(global.debugMode){
+  // if(global.debugMode){
     body = '지사코드 : ' + area + '\n' + body;
-  }
+  // }
 
   // Configure the request
   var options = {
@@ -63,7 +64,7 @@ exports.sendPush = function(type, area, summary, callback){
       form: {
         'uuid' : global.debugMode ? testPush.uuid : uuid,
         'secret_key' : global.debugMode ? testPush.secret_key : secret_key,
-        'code' : global.debugMode ? 'neotest0000' : 'neosoftbank' + area,
+        'code' : global.debugMode ? 'neotest0000' : typeof summary === 'undefined' ? 'neotest0000' : 'neosoftbank' + area,
         'body' : body
       }
   };
